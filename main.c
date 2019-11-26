bec8563c46662dedf5e2e45c6a638e00da1dfc97
minkyeong417 <55428050+minkyeong417@users.noreply.github.com>
2019-11-26 15:32:18 +0900

    0101

--- main.c
+++ main.c
@@ -16,6 +16,13 @@ int main(int argc, char *argv[]) {
 	
 	struct student s1 = {24, "minkyeong",4.3	};
 	
+	s1.id = 1710153;
+	strcpy(s1.name,"mingyeong");
+	s1.grade=4.5;
+	
+	printf("id: %d\n",s1.id);
+	printf("name: %s\n",s1.name);
+	printf("grade: %f\n",s1.grade);
 	
 	return 0;
 }
